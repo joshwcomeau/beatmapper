1a4aed102bbec78896786d50ce23874468918694
Josh Comeau <jcomeau@digitalocean.com>
2019-09-23 12:25:41 -0400

    pre-eject stuff

--- src/middlewares/backup.middleware.js
+++ src/middlewares/backup.middleware.js
@@ -1,38 +1,18 @@
-import { saveBeatmap } from '../services/file.service';
-import { createBeatmapContentsFromState } from '../services/packaging.service';
-import { getSelectedSong } from '../reducers/songs.reducer';
-import { getDifficulty } from '../reducers/editor-entities.reducer';
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import worker from 'workerize-loader!../workers/autosave';
+
+console.log(worker);
+const instance = worker();
+console.log(instance);
 
 export default function createBackupMiddleware() {
   return store => next => action => {
     next(action);
 
-    // A mechanism already exists to back up the Redux state to our persistence
-    // layer, so that the state can be rehydrated on return visits.
-    //
-    // The only Redux state persisted is the `songs` reducer; for stuff like
-    // what the notes are for the current song, we'll read that from the files
-    // saved to disk, stuff like `songName_info.dat` and `songName_Expert.dat`.
-    //
-    // Whenever redux saves the song list, we should also save all the stuff
-    // in the editor-entities reducer by storing them in info files.
-    //
-    // (If this feels overly complicated, and you're wondering why I don't just
-    // store _everything_ in redux, it's because the user can have dozens or
-    // hundreds of songs, and each song can have thousands of notes. It's too
-    // much to keep in RAM. So I store non-loaded songs to disk, stored in
-    // indexeddb. It uses the same mechanism as Redux Storage, but it's treated
-    // separately.)
     if (action.type === 'REDUX_STORAGE_SAVE') {
-      console.log('save!');
       const state = store.getState();
 
-      const song = getSelectedSong(state);
-      const difficulty = getDifficulty(state);
-      const beatmapContents = createBeatmapContentsFromState(state, song);
-      saveBeatmap(song.id, difficulty, beatmapContents).catch(err => {
-        console.error('Could not run backup for beatmap file', err);
-      });
+      instance.save(state);
     }
   };
 }
---
+++ src/workers/autosave.worker.js
@@ -0,0 +1,30 @@
+import { saveBeatmap } from '../services/file.service';
+import { createBeatmapContentsFromState } from '../services/packaging.service';
+import { getSelectedSong } from '../reducers/songs.reducer';
+import { getDifficulty } from '../reducers/editor-entities.reducer';
+
+// A mechanism already exists to back up the Redux state to our persistence
+// layer, so that the state can be rehydrated on return visits.
+//
+// The only Redux state persisted is the `songs` reducer; for stuff like
+// what the notes are for the current song, we'll read that from the files
+// saved to disk, stuff like `songName_info.dat` and `songName_Expert.dat`.
+//
+// Whenever redux saves the song list, we should also save all the stuff
+// in the editor-entities reducer by storing them in info files.
+//
+// (If this feels overly complicated, and you're wondering why I don't just
+// store _everything_ in redux, it's because the user can have dozens or
+// hundreds of songs, and each song can have thousands of notes. It's too
+// much to keep in RAM. So I store non-loaded songs to disk, stored in
+// indexeddb. It uses the same mechanism as Redux Storage, but it's treated
+// separately.)
+export default function autosave(state) {
+  console.log('Autosave!');
+  const song = getSelectedSong(state);
+  const difficulty = getDifficulty(state);
+  const beatmapContents = createBeatmapContentsFromState(state, song);
+  saveBeatmap(song.id, difficulty, beatmapContents).catch(err => {
+    console.error('Could not run backup for beatmap file', err);
+  });
+}
