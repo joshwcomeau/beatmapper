e608d200f592c13fb98555c5d9a0de50d57cec30
Josh Comeau <joshwcomeau@gmail.com>
2019-10-05 17:09:13 +0100

    Start work on a few docs

--- src/components/Docs/Layout.js
+++ src/components/Docs/Layout.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { COLORS } from '../../constants';
-
 import Sidebar from './Sidebar';
 import SearchHeader from './SearchHeader';
 
