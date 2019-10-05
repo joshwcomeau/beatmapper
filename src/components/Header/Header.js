3fc43eb1fdf33c789853977aaccaa25b7e888a95
Josh Comeau <joshwcomeau@gmail.com>
2019-10-05 15:24:48 +0100

    meta-docs stuff

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
+import BaseLink from '../BaseLink';
 
-import { HEADER_HEIGHT } from '../../constants';
+import { COLORS, HEADER_HEIGHT } from '../../constants';
 
 import MaxWidthWrapper from '../MaxWidthWrapper';
 import Logo from '../Logo';
@@ -12,6 +13,7 @@ const Header = () => {
       <MaxWidthWrapper>
         <Contents>
           <Logo />
+          <DocLink to="/docs">Documentation</DocLink>
         </Contents>
       </MaxWidthWrapper>
     </Wrapper>
@@ -21,11 +23,21 @@ const Header = () => {
 const Wrapper = styled.div`
   height: ${HEADER_HEIGHT}px;
   line-height: ${HEADER_HEIGHT}px;
-  background: hsla(222, 10%, 92%, 0.025);
+  background: ${COLORS.blueGray[900]};
 `;
 
 const Contents = styled.div`
   display: flex;
+  justify-content: space-between;
+`;
+
+const DocLink = styled(BaseLink)`
+  text-decoration: none;
+  color: white;
+
+  &:hover {
+    text-decoration: underline;
+  }
 `;
 
 export default Header;
