1559ac9962384ee3b8412f1e6a3baf4ff8a87f0b
Josh Comeau <joshwcomeau@gmail.com>
2019-10-05 15:16:00 +0100

    Work on Docs sidebar

--- src/components/Docs/Sidebar.js
+++ src/components/Docs/Sidebar.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
-import { NavLink } from 'react-router-dom';
+import { NavLink as NavLinkRaw } from 'react-router-dom';
 
 import { COLORS } from '../../constants';
 
 import Logo from '../Logo';
+import NavGroup from './NavGroup';
 
 const Sidebar = () => {
   return (
@@ -13,14 +14,22 @@ const Sidebar = () => {
         <Logo color="#000" />
       </Header>
       <Navigation>
-        <NavLink to="/docs">About</NavLink>
-        <NavGroup title="User Manual">
+        <NavGroup>
+          <NavLink exact to="/docs">
+            About
+          </NavLink>
+        </NavGroup>
+        <NavGroup title="User Manual" showByDefault>
           <NavLink to="/docs/getting-started">Getting started</NavLink>
           <NavLink to="/docs/notes">Notes view</NavLink>
           <NavLink to="/docs/events">Events view</NavLink>
           <NavLink to="/docs/packaging">Downloading and publishing</NavLink>
         </NavGroup>
-        <NavLink to="/docs/keyboard-shortcuts">Keyboard shortcuts</NavLink>
+        <NavGroup>
+          <NavLink to="/docs/keyboard-shortcuts">Keyboard shortcuts</NavLink>
+          <NavLink to="/docs/content-policy">Content policy</NavLink>
+          <NavLink to="/docs/privacy">Privacy</NavLink>
+        </NavGroup>
       </Navigation>
     </Wrapper>
   );
@@ -31,7 +40,32 @@ const Wrapper = styled.div`
   border-right: 1px solid ${COLORS.blueGray[100]};
 `;
 
-const Navigation = styled.nav``;
+const Navigation = styled.nav`
+  padding-left: 12px;
+`;
+
+const NavLink = styled(NavLinkRaw).attrs(props => ({
+  activeStyle: {
+    color: COLORS.blue[500],
+  },
+}))`
+  display: flex;
+  align-items: center;
+  height: 35px;
+  color: ${COLORS.blueGray[700]};
+  font-family: 'system';
+  font-weight: 500;
+  font-size: 16px;
+  text-decoration: none;
+
+  &:hover {
+    color: ${COLORS.blueGray[400]};
+  }
+`;
+
+const ActiveNavLink = styled(NavLink)`
+  color: ${COLORS.blue[500]};
+`;
 
 const Header = styled.header`
   height: 80px;
@@ -40,7 +74,5 @@ const Header = styled.header`
   align-items: center;
   padding: 0 20px;
 `;
-
-const NavGroup = styled.div``;
 
 export default Sidebar;
