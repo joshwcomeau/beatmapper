5f0921b2d87b10386d437607d8428cb83e9c9392
Josh Comeau <joshwcomeau@gmail.com>
2019-10-06 08:11:30 +0100

    Add active style for TableOfContents

--- src/components/Docs/TableOfContents.js
+++ src/components/Docs/TableOfContents.js
@@ -2,18 +2,53 @@ import React from 'react';
 import styled from 'styled-components';
 
 import { COLORS } from '../../constants';
-import Heading from '../Heading';
+import { throttle } from '../../utils';
 
-const TableOfContents = ({ title, toc }) => {
-  console.log(toc);
+const useActiveHeading = headings => {
+  const [activeHeading, setActiveHeading] = React.useState(headings[0]);
+
+  React.useEffect(() => {
+    const handleScroll = throttle(() => {
+      // The first heading within the viewport is the one we want to highlight.
+      const firstHeadingInViewport = [...headings].reverse().find(({ id }) => {
+        const elem = document.querySelector(`#${id}`);
+        const bb = elem.getBoundingClientRect();
+
+        return bb.bottom < window.innerHeight;
+      });
+
+      if (firstHeadingInViewport !== activeHeading) {
+        setActiveHeading(firstHeadingInViewport);
+      }
+    }, 500);
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [headings]);
+
+  return activeHeading;
+};
+
+const TableOfContents = ({ toc }) => {
   const headings = toc.filter(item => item.level <= 4);
+
+  const activeHeading = useActiveHeading(headings);
 
   return (
     <Wrapper>
-      <Title>{title}</Title>
+      <Title>Table of Contents</Title>
 
       {headings.map(({ title, id }) => (
-        <HeadingLink key={id} href={`#${id}`}>
+        <HeadingLink
+          key={id}
+          href={`#${id}`}
+          style={{
+            color: id === activeHeading.id ? COLORS.pink[700] : undefined,
+          }}
+        >
           {title}
         </HeadingLink>
       ))}
@@ -23,10 +58,10 @@ const TableOfContents = ({ title, toc }) => {
 
 const Wrapper = styled.div`
   width: 180px;
-  margin-left: 40px;
   line-height: 1.4;
   position: sticky;
   top: 100px;
+  margin-left: 40px;
 
   @media (max-width: 1199px) {
     display: none;
@@ -34,9 +69,11 @@ const Wrapper = styled.div`
 `;
 
 const Title = styled.h4`
-  font-size: 18px;
+  font-size: 16px;
   font-weight: 600;
-  margin-bottom: 32px;
+  margin-bottom: 12px;
+  padding-bottom: 12px;
+  border-bottom: 1px solid ${COLORS.blueGray[100]};
 `;
 
 const HeadingLink = styled.a`
