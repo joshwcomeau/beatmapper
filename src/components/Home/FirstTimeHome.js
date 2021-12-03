240901a084531cdf4b44a2db65b5a409bd03e772
Joshua Comeau <joshwcomeau@gmail.com>
2021-12-03 10:16:20 -0500

    Move to Vimeo. Remove demo map

--- src/components/Home/FirstTimeHome.js
+++ src/components/Home/FirstTimeHome.js
@@ -10,7 +10,6 @@ import * as actions from '../../actions';
 import { COLORS, UNIT } from '../../constants';
 import useWindowDimensions from '../../hooks/use-window-dimensions.hook';
 import { getDemoSong } from '../../reducers/songs.reducer';
-import heroVideo from '../../assets/videos/hero-video.mp4';
 
 import Spacer from '../Spacer';
 import Heading from '../Heading';
@@ -35,37 +34,31 @@ const FirstTimeHome = ({ loadDemoMap, setModal, demoSong, history }) => {
           Beatmapper is an unofficial web-based editor for Beat Saber™
         </Title>
         <Spacer size={UNIT * 4} />
-        <video
-          src={heroVideo}
-          autoPlay
-          muted
-          loop
-          controls
-          style={{
-            width: videoWidth,
-            marginLeft: -WRAPPER_PADDING,
-            marginRight: -WRAPPER_PADDING,
-          }}
-        />
+        <div
+          style={{ width: '100%', padding: '75% 0 0 0', position: 'relative' }}
+        >
+          <iframe
+            src="https://player.vimeo.com/video/652869239?h=5b27065edf&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+            frameborder="0"
+            allow="autoplay; fullscreen; picture-in-picture"
+            allowfullscreen
+            style={{
+              position: 'absolute',
+              top: '0',
+              left: '0',
+              width: '100%',
+              height: '100%',
+            }}
+            title="hero-video.mp4"
+          ></iframe>
+        </div>
+        <script src="https://player.vimeo.com/api/player.js"></script>
 
         <Spacer size={UNIT * 10} />
         <Heading size={2}>Get started now</Heading>
       </Center>
       <Spacer size={UNIT * 6} />
       <Row>
-        <OptionColumn
-          icon={box}
-          title="Try a demo map"
-          description="Take the editor for a test-drive with some surprisingly good public-domain dubstep"
-          buttonText={isLoadingDemo ? 'Loading…' : 'Start editing'}
-          handleClick={() => {
-            if (!demoSong) {
-              setIsLoadingDemo(true);
-              loadDemoMap();
-            }
-          }}
-        />
-        <Divider />
         <OptionColumn
           icon={filePlus}
           title="Create new song"
--- src/middlewares/demo.middleware.js
+++ src/middlewares/demo.middleware.js
@@ -2,33 +2,36 @@
  * This middleware exists only to load (and possibly manage) the demo song
  * that comes with this app.
  */
-import demoFileUrl from '../assets/demo/demo-map.zip';
 import { importExistingSong } from '../actions';
 import { processImportedMap } from '../services/packaging.service';
 import { getIsNewUser } from '../reducers/user.reducer';
+
+// import demoFileUrl from '../assets/demo/demo-map.zip';
 
 export default () => (store) => (next) => {
   return (action) => {
     next(action);
 
-    if (action.type === 'LOAD_DEMO_MAP') {
-      // If this is a brand-new user, they won't have the demo song at all
-      const state = store.getState();
-      const isNewUser = getIsNewUser(state);
+    alert('Sorry, I had to remove the demo map');
 
-      if (isNewUser) {
-        fetch(demoFileUrl)
-          .then((res) => res.blob())
-          .then((blob) => processImportedMap(blob, []))
-          .then((songData) => {
-            songData.demo = true;
-            next(importExistingSong(songData));
-          })
-          .then(() => {
-            // HACK: Should pull data from demoSong
-            window.location = '/edit/only-now/Normal/notes';
-          });
-      }
-    }
+    // if (action.type === 'LOAD_DEMO_MAP') {
+    //   // If this is a brand-new user, they won't have the demo song at all
+    //   const state = store.getState();
+    //   const isNewUser = getIsNewUser(state);
+
+    //   if (isNewUser) {
+    //     fetch(demoFileUrl)
+    //       .then((res) => res.blob())
+    //       .then((blob) => processImportedMap(blob, []))
+    //       .then((songData) => {
+    //         songData.demo = true;
+    //         next(importExistingSong(songData));
+    //       })
+    //       .then(() => {
+    //         // HACK: Should pull data from demoSong
+    //         window.location = '/edit/only-now/Normal/notes';
+    //       });
+    //   }
+    // }
   };
 };
