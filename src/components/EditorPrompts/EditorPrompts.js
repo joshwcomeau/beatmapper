49e36d7e737e0a22747d99da11b354fd2fd2a2dd
Joshua Comeau <joshwcomeau@gmail.com>
2021-12-07 10:49:57 -0500

    Add new warning

--- src/components/EditorPrompts/EditorPrompts.js
+++ src/components/EditorPrompts/EditorPrompts.js
@@ -12,77 +12,30 @@ import { InlineIcons, KeyIcon, MetaKey } from '../Docs/ShortcutHelpers';
 
 const PROMPTS = [
   {
-    id: 'alpha-warning',
-    title: 'Welcome!',
+    id: 'deprecated',
+    title: 'This editor is deprecated.',
     contents: () => (
       <>
         <Paragraph>
-          Hi there, new user! Two important things to know before you get
-          started:
+          Hey there — so, Beatmapper isn't really maintained anymore, and you're
+          probably better off switching to another editor.
+        </Paragraph>
+        <Paragraph>
+          Check out{' '}
+          <a
+            href="https://github.com/Caeden117/ChroMapper"
+            style={{ color: 'inherit' }}
+          >
+            ChroMapper
+          </a>
+          . It's a free, cross-platform editor which is regularly updated. It
+          supports 90/360 map creation as well!
         </Paragraph>
 
-        <List>
-          <List.ListItem>
-            Beatmapper is <em>alpha software</em>. That means stuff might break
-            at any moment. <strong>Download your work frequently</strong> using{' '}
-            <InlineIcons>
-              <KeyIcon type="slightly-wide" size="small">
-                <MetaKey />
-              </KeyIcon>
-              <KeyIcon size="small">S</KeyIcon>
-            </InlineIcons>
-          </List.ListItem>
-
-          <List.ListItem>
-            We have really thorough docs! Please{' '}
-            <strong>
-              <Link to="https://beatmapper.app/docs/docs/manual.html">
-                check them out
-              </Link>
-            </strong>
-            , there's lots of good info in there.
-          </List.ListItem>
-        </List>
-
         <Paragraph>
-          <span role="img" aria-label="rainbow">
-            🌈
-          </span>{' '}
-          Have fun!{' '}
-          <span role="img" aria-label="sparkles">
-            ✨
-          </span>
-        </Paragraph>
-      </>
-    ),
-  },
-  {
-    id: 'version-zero-point-three',
-    title: 'Version 0.3 released!',
-    contents: () => (
-      <>
-        <Paragraph>
-          We just shipped a new version, with some pretty exciting new stuff:
-        </Paragraph>
-
-        <List>
-          <List.ListItem>
-            You can <strong>preview your lights</strong> now, right from the
-            Events page!
-          </List.ListItem>
-
-          <List.ListItem>
-            Having performance problems? Switch to a{' '}
-            <strong>low-quality graphics setting</strong>.
-          </List.ListItem>
-        </List>
-
-        <Paragraph>
-          Learn more about these two new features in our{' '}
-          <strong>
-            <Link to="/docs/release-notes">release notes</Link>
-          </strong>
-          .
+          You're welcome to keep using Beatmapper, but please know that{' '}
+          <strong>you're doing so at your own risk.</strong> It's possible that
+          the maps you create won't work in-game, as the game updates.
         </Paragraph>
       </>
     ),
@@ -104,10 +57,10 @@ const EditorPrompts = ({ prompt, dismissPrompt }) => {
   );
 };
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   const seenPrompts = getSeenPrompts(state);
   const unseenPrompts = PROMPTS.filter(
-    prompt => !seenPrompts.includes(prompt.id)
+    (prompt) => !seenPrompts.includes(prompt.id)
   );
 
   return {
@@ -117,7 +70,4 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = { dismissPrompt: actions.dismissPrompt };
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(EditorPrompts);
+export default connect(mapStateToProps, mapDispatchToProps)(EditorPrompts);
